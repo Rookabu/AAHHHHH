6668faeaed28f100d7facdc087ce950c320642da
Rookabu <annika-paul1@gmx.de>
2024-02-21 14:31:06 +0100

    navbar erstellt

--- src/startNavBar.fs
+++ src/startNavBar.fs
@@ -3,4 +3,33 @@ namespace App
 open Feliz
 open Feliz.Router
 
-//type Components =
+type navComponents =
+    [<ReactComponent>]
+
+    static member Counter() = 
+        
+        Html.div [
+            Html.a [
+                prop.text "Cooler Counter"
+                prop.className "todoliste"
+                prop.href "https://www.youtube.com/"
+            ]
+        ]
+
+    static member Todo() =     
+        Html.div [
+            Html.a [ 
+                prop.text "Freshe To-Do-Liste"
+                prop.className "todoliste"
+                prop.href "https://www.youtube.com/"
+            ]
+        ]
+    /// A React component that uses Feliz.Router
+    /// to determine what to show based on the current URL
+    /// </summary>
+
+
+        
+        
+
+    
--- src/todo.fs
+++ src/todo.fs
@@ -3,4 +3,18 @@ namespace App
 open Feliz
 open Feliz.Router
 
-//type Components =
+type Components =
+    [<ReactComponent>]
+
+    static member Todo () = 
+        Html.div [
+
+            
+        ]
+
+
+          // Html.a [
+            //     prop.text "Freshe To-Do-Liste"
+            //     prop.className "todoliste"
+            //     prop.href "https://www.youtube.com/"
+            // ]     
