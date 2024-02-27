2fd33ad28118e5214dc913d86696cfb1b21fc01f
Rookabu <annika-paul1@gmx.de>
2024-02-27 15:38:12 +0100

    Nav bar gefixxed

--- src/startNavBar.fs
+++ src/startNavBar.fs
@@ -2,7 +2,6 @@
 namespace Components
 
 open Feliz
-open Feliz.Router
 open Feliz.Bulma
 
 //Definition der NavBar und design (css datei)
@@ -11,52 +10,60 @@ open Feliz.Bulma
 
 type NavBar =
     //let mutable ungeklickt = (prop.className "remainHighlight")  
-    static member private Subpagelink(targetpage: Types.Page, setPage: Types.Page -> unit, statePage: Types.Page) = //öffnet etwas beim click und gibt den text an von toStingreadable
-        Bulma.navbarMenu [
-            Bulma.navbarStart.div [
-                Bulma.navbarItem.a [
-                    prop.text (targetpage.toStringReadable());   
-                    prop.onClick (fun _ -> setPage(targetpage))
-                    if targetpage = statePage then  //target page = gewollter state, statepage= aktueller state                
+    static member private Subpagelink(setPage: Types.Page -> unit, statePage: Types.Page) = //öffnet etwas beim click und gibt den text an von toStingreadable
+        Bulma.navbarMenu [                        
+            Bulma.navbarStart.div [  
+                Bulma.navbarItem.a [ Html.img [ prop.src "https://bulma.io/images/bulma-logo-white.png"; prop.height 28; prop.width 112; ]]
+     
+                Bulma.navbarItem.a [                
+                    prop.text "Nav Bar";   
+                    prop.className "ofa_buttonmenü"                                
+                ]             
+                Bulma.navbarItem.a [                
+                    prop.text "Todo-Liste";   
+                    prop.onClick (fun _ -> setPage(Types.Page.Todo))
+                    if Types.Page.Todo = statePage then  //target page = gewollter state, statepage= aktueller state                
                         prop.className "remainHighlight"   
-                    else prop.className "ofa_button"
-                                
+                    else prop.className "ofa_button"                                
                 ]
-            ]
-        // ] //Definiert den subpage link und ihr aussehen (ofa button)
+                Bulma.navbarItem.a [                
+                    prop.text "Counter";   
+                    prop.onClick (fun _ -> setPage(Types.Page.Counter))
+                    if Types.Page.Counter = statePage then  //target page = gewollter state, statepage= aktueller state                
+                        prop.className "remainHighlight"   
+                    else prop.className "ofa_button"                                
+                ]
+            ]  
+        ]  
+         
+        
+        //Definiert den subpage link und ihr aussehen (ofa button)
+
         // Bulma.navbarMenu [
         //     Bulma.navbarStart.div [
-        //         Bulma.navbarItem.a [ prop.text "Home"]                
-        //         Bulma.navbarItem.a [ prop.text "Counter" ]
-        //         Bulma.navbarItem.a [ prop.text "Todo-Liste" ]
-        //         Bulma.navbarItem.div [
-        //             Bulma.navbarItem.hasDropdown
-        //             Bulma.navbarItem.isHoverable
-        //             prop.children [
-        //                 Bulma.navbarLink.a [ prop.text "More" ]
-        //                 Bulma.navbarDropdown.div [
-        //                     Bulma.navbarItem.a [ prop.text "About" ]
-        //                     Bulma.navbarItem.a [ prop.text "Contact" ]
-        //                     Bulma.navbarDivider []
-        //                     Bulma.navbarItem.a [ prop.text "Report a issue" ]
+        //         Bulma.navbarItem.a [prop.text (targetpage.toStringReadable()); 
+        //                             prop.onClick (fun _ -> setPage(targetpage));
+        //                             if targetpage = statePage then  //target page = gewollter state, statepage= aktueller state                
+        //                                 prop.className "remainHighlight"   
+        //                             else prop.className "ofa_button"]                
         //                 ]
         //             ]
-        //         ]
-        //     ]
-        ]
+        
+
 
     
-    static member Counter(setPage, currentpage) = 
-        NavBar.Subpagelink(Types.Page.Counter, setPage, currentpage)   //verlinkt den richten suppage link mit dem typen
+    // static member Counter(setPage, currentpage) = 
+    //     NavBar.Subpagelink( setPage, currentpage)   //verlinkt den richten suppage link mit dem typen
      
+    static member Allgemein(setPage, currentpage) =   
+        NavBar.Subpagelink(setPage, currentpage)  //verlinkt den richten suppage link mit dem typen
 
-    static member Todo(setPage, currentpage) =   
-        NavBar.Subpagelink(Types.Page.Todo, setPage, currentpage)  //verlinkt den richten suppage link mit dem typen
     // static member Überschrift() =
-    //     Html.div[
-    //         prop.text "Navigier Bar"
-    //         prop.className "ueberschrift"
-    //     ]     
+    //      Bulma.navbarMenu [
+    //         Bulma.navbarStart.div [
+    //             Bulma.navbarItem.a [prop.text "NavBar"; prop.className "ofa_buttonmenü"]
+    //         ]
+    //     ]
 
 
 
--- src/view.fs
+++ src/view.fs
@@ -1,7 +1,6 @@
 namespace App
 
 open Feliz
-open Feliz.Router
 
 //Ist beim aufrufen der Seite sichtbar und ruft den Counter auf mit der navbar.todo
 type View =
@@ -10,16 +9,18 @@ type View =
         let currentpage,setpage = React.useState(Types.Page.Counter) //Reagiert beim clicken. Start state ist der Counter ->noch in Srat menü umändern
         printfn "%A" currentpage //wird in console geprinted
         Html.div [
-            prop.children [
-                Html.nav [               
-                    Components.NavBar.Todo(setpage,currentpage)
-                    Components.NavBar.Counter(setpage, currentpage)
+            prop.children [ 
+                Html.a [ 
+                    //Components.NavBar.Überschrift()          
+                    Components.NavBar.Allgemein(setpage,currentpage)
+                    //Components.NavBar.Counter(setpage, currentpage)
                 ]
                 match currentpage with
                 |Types.Page.Counter -> Components.Counter.Main()
                 |Types.Page.Todo -> Components.Todo.Main() //wird gemachted da die jeweilige component aufgerufen werden soll je nach page (application state)
             ]
         ]
+
 
 
 
