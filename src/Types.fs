f849973ea63abe960a9b73059373edbc5a0673bb
Rookabu <annika-paul1@gmx.de>
2024-02-28 14:59:00 +0100

    Todo versucht

--- src/Types.fs
+++ src/Types.fs
@@ -4,10 +4,10 @@ module Types
 type Page =
     |Counter
     |Todo
-    member this.toStringReadable () =
-        match this with
-        |Counter -> "Counter" 
-        |Todo -> "Todo-Liste" //zu einem lesbaren string umwandeln (wird angezeigt auf der website) 
+    // member this.toStringReadable () =
+    //     match this with
+    //     |Counter -> "Counter" 
+    //     |Todo -> "Todo-Liste" //zu einem lesbaren string umwandeln (wird angezeigt auf der website) 
 
 
 
--- src/todo.fs
+++ src/todo.fs
@@ -6,17 +6,48 @@ open Feliz.Bulma
  
 //Hier ist die Todo definiert
 
+
+type Listenelement = {
+    Eintrag: string
+    Checkbox: bool     
+}
+
 type Todo =
     [<ReactComponent>]
     static member Main() = 
-        //let (table, addtableslot) = React.useState()
-        Bulma.tag[
-            prop.text "Erstelle dir eine Todo-Liste!"
-            prop.style [
-                style.margin (length.rem 1)
-                style.fontSize 20
-            ]
+        let recordtypeliste = [
+            {Eintrag = "Beispiel: Einkaufen gehen"; Checkbox = false}
         ]
+        let (table:Listenelement list), addtable = React.useState(recordtypeliste) //state: liste aus record typen //todoliste: string + bool
+        Html.div [
+            Bulma.tag[
+                color.isLink
+                prop.text "Erstelle dir eine Todo-Liste! Du kannst diese abhaken und wieder entfernen."
+                prop.style [
+                    style.margin (length.rem 1)
+                    style.fontSize 20
+                ]
+            ]
+            Html.h1 [
+                Bulma.button.button [
+                    color.isInfo;
+                    prop.text "Eintrag hinzufügen"
+                    //prop.onClick (fun _ -> addtableslot(Listenelement))
+                    prop.style [
+                        style.margin (length.rem 1)
+                        style.fontSize 20
+                    ]
+                ]
+            ]
+        //     Bulma.tag[
+        //         prop.text table
+        //         prop.style [
+        //             style.margin (length.rem 1)
+        //             style.fontSize 20
+        //         ]
+        //     ]
+         ]
+
 
 
 
